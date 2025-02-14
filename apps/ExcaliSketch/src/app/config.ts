340133127a2bb9618d861c871f67a012d2d06230
shahzan01 <m.shahzan2003@gmail.com>
2025-02-14 10:11:34 +0530

    backend url updated

--- apps/ExcaliSketch/src/app/config.ts
+++ apps/ExcaliSketch/src/app/config.ts
@@ -3,5 +3,7 @@ import path from "path";
 dotenv.config({
   path: path.resolve(__dirname, ".env"),
 });
-export const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:5000";
-export const WS_URL = process.env.WS_URL || "ws://localhost:5000";
+export const BACKEND_URL =
+  process.env.BACKEND_URL || "https://excali-sketch-api.onrender.com";
+export const WS_URL =
+  process.env.WS_URL || "https://excali-sketch-api.onrender.com";
