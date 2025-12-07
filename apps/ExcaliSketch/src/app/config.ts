0d294c9b8591ab713ccbcc08062c51d43c15bb57
shahzan01 <m.shahzan2003@gmail.com>
2025-12-07 12:14:43 +0530

    removed dot env

--- apps/ExcaliSketch/src/app/config.ts
+++ apps/ExcaliSketch/src/app/config.ts
@@ -1,8 +1,3 @@
-import dotenv from "dotenv";
-import path from "path";
-
-dotenv.config();
-
 export const BACKEND_URL =
   (process.env.NEXT_PUBLIC_BACKEND_URL as string) || "http://localhost:5000";
 export const WS_URL =
